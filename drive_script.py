62bed80d8c7ef1dfa56b6304230f4b72ce51ff32
trinity <trinity>
2023-07-20 06:26:06 +0000

    script works

--- drive_script.py
+++ drive_script.py
@@ -1,11 +1,11 @@
-import storm_lib
+from storm_lib import storm
 import time
 
 
 if __name__ == '__main__':
-    robot = storm_lib.Storm()
+    robot = storm.Storm()
     for i in range(0,100):
         robot.set_vel(0.1,0.0)
-        time.sleep(100)
+        time.sleep(1)
         robot.set_vel(0.0,0.0)
-        time.sleep(100)
+        time.sleep(1)
