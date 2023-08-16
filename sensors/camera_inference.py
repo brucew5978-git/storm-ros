d4aeb50da689a4aa06028ef040000e0e8b3454ca
brucewang <brucew5978@gmail.com>
2023-08-16 14:44:01 -0400

    camera odom initialize

---
+++ sensors/camera_inference.py
@@ -0,0 +1,7 @@
+#Will perform inference on the camera feed and return the results
+# of the inference. This will be used to determine the objects pitch and yaw
+
+import time, math
+
+def get_pitch_yaw():
+    return 0, 0
